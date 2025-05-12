75340b7a1c482370ce880208c5c17aae0f9539d2
Matias <lit.io30303@gmail.com>
2025-05-12 17:58:29 -0400

    #

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -11,13 +11,13 @@ export default async function page() {
   return (
     <div className="grid sm:overflow-auto md:grid-cols-2 h-full max-h-screen overflow-hidden">
       <div className="flex justify-center h-full ">
-        <div className="text-white flex flex-col items-center justify-center">
-          <h1 className="text-blue-500 text-2xl text-center mb-5">
+        <div className="text-white flex flex-col items-center justify-center sm:px-8">
+          <h1 className="text-blue-500 text-2xl text-center mb-3 sm:mb-5">
             GestorPass
           </h1>
           <h2 className="text-4xl font-medium text-black">Bienvenidos</h2>
-          <p className="text-center mt-4 mb-6 text-slate-400 text-sm">
-            Bienvenidos, porfavor ingrese los detalles
+          <p className="text-center mt-2 sm:mt-4 mb-4 sm:mb-6 text-slate-400 text-xs sm:text-sm">
+            Bienvenidos, por favor ingrese los detalles
           </p>
           <TabsForms></TabsForms>
         </div>
