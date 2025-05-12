da61d627d2394c0a36262998e85f8a9269bde495
Matias <lit.io30303@gmail.com>
2025-05-12 14:57:00 -0400

    actualizando route.ts

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -2,15 +2,28 @@ import { db } from "@/lib/db";
 import { hash } from "bcrypt";
 import { NextResponse } from "next/server";
 
+// async function checkDB() {
+//     try {
+//         const users = await db.user.findMany();
+//         console.log(users);
+//     } catch (error) {
+//         console.error("Error conectando a la BD:", error);
+//     }
+// }
+
+// checkDB();
+
+
+
 export async function POST(req: Request) {
     try {
         const { email, password, username } = await req.json();
         const hashedPassword = await hash(password, 10);
 
-        if (!email || !password) {
-            return new NextResponse("Missing required fields", { status: 400 })
-
+        if (!email || !password || !username) {
+            return NextResponse.json({ error: "Faltan datos obligatorios" }, { status: 400 });
         }
+
         const user = await db.user.create({
             data: {
                 email,
@@ -18,9 +31,12 @@ export async function POST(req: Request) {
                 username
             }
         });
-        return NextResponse.json(user);
+        return NextResponse.json(user, { status: 201 });
+
 
     } catch (error) {
+        console.error("Error al registrar usuario:", error);
+
         return new NextResponse(`Internal Error: ${error}`, { status: 500 })
     }
 }
