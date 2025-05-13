fd0f4fd2a98152535018147a9489e73de6dbb2f2
Matias <lit.io30303@gmail.com>
2025-05-13 18:40:57 -0400

    add manifest

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,7 @@ const poppins = Poppins({
 export const metadata: Metadata = {
   title: "Gestpass",
   description: "App para gestionar contraseñas",
+  manifest:"/manifest.json"
 };
 
 export default function RootLayout({
